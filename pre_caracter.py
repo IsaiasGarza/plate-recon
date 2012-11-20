5efbe714f78bf48281e430d3df5872be42f2e2e6
Emmanuel <aleksgs10@gmail.com>
2012-11-19 21:33:37 -0600

    Localizacion y segmentacion de caracteres

--- pre_caracter.py
+++ pre_caracter.py
@@ -14,7 +14,7 @@ import ImageTk, Image
 import Tkinter as tk
 import math
 
-element = cv2.getStructuringElement(cv2.MORPH_CROSS,(9,8))
+element = cv2.getStructuringElement(cv2.MORPH_CROSS,(7,5))
 input_img = argv[1]
 img = cv2.imread(input_img)
 
@@ -53,16 +53,17 @@ def verificar_pixeles(old, y):
   pix = old.load()
   x, x1 = 0, w
   for i in range(x, x1):
+    #print i, y
     if pix[i, y] == 255:
 	return False
   return True
 	  
 
 
-def escala():
+def cortar_imagen():
   old = Image.open('output.png')
-  print old.mode
   #print old.mode
+  ##print old.mode
   w, h = old.size
   root = tk.Tk()
   pix = old.load()
@@ -70,34 +71,42 @@ def escala():
   x1, x2 = 0, w
   y1, y2 = 0, 0
   done = False
+  d_a = False
+  d_b = False
   for i in range(w/2):
     for j in range(h/2 - 1):
       if verificar_pixeles(old, a):
 	y1 = a
+	d_a = True
       else:
 	a += 1
+	if a >= h: a, d_a = h-1, True
       if verificar_pixeles(old, b):
 	y2 = b
+	d_b = True
       else:
         b -= 1
-      if y1 > 0 and y2 > 0:
+	if b < 0: b, d_b = 0, True
+      if d_a and d_b:
 	done = True
 	break
     if done:
 	break
      
   box = (x1, y2, x2, y1)      
-  print box
+  #print box
   old = old.crop(box)
+  w, h = old.size
+  #old = old.resize((w, 70), Image.NEAREST)
   old.save('output.png', 'PNG')
   return box
 
 def close(x, w, x1, x2):
   if x1 in range (x-5, x+5):
-    print 'yes'
+    #print 'yes'
     return True
   elif x2 in range (w-5, w+5):
-    print 'yes1'
+    #print 'yes1'
     return True
   else:
     return False
@@ -110,7 +119,7 @@ def main():
   img = threshold_image(img, 128, cv2.THRESH_BINARY_INV)
   img = erode_image(img)
   cv2.imwrite('output.png', img)
-  x, y, w, h = escala()
+  cortar_imagen()
   img = cv2.imread('output.png')
   gray = convert_grayscale(img)
   contours, hierarchy = cv2.findContours(gray, mode=cv2.RETR_LIST,method=cv2.CHAIN_APPROX_SIMPLE )
@@ -120,21 +129,23 @@ def main():
     x,y,w,h = cv2.boundingRect(contour)
     w = w + x
     h = h + y
+    #print h-y
     if (h-y) > 60:
-      print x, y, w, h
+      ##print x, y, w, h
       rects.append([x, y, w, h])
   i = 0
-
-
-
+  rects.sort()
   for rect in rects:
     x, y, w, h = rect[0], rect[1], rect[2], rect[3]
-    #cv2.rectangle(img,(x,y),(w,h),(255),2)
+    #cv2.rectangle(img,(x,y),(w,h),(255, 0, 0),2)
     cv2.imwrite('output_%s.png'%i, img[y:h, x:w])
     i+=1
-  
-
-
+  cv2.imwrite('output.png', img)
+  for k in range(len(rects)):
+    im = Image.open('output_%s.png'%k)
+    im2 = im.resize((30, 70), Image.NEAREST)
+    ##print im2.size
+    im2.save('output_%s.png'%k, 'PNG')
 
 main()
 

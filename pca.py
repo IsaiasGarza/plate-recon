9c6c576202b514afe7df1df8c787f22473f2284f
Emmanuel <aleksgs10@gmail.com>
2012-11-19 18:20:43 -0600

    PCA para obtener datos de imagenes de caracteres adaptado a neurona

--- pca.py
+++ pca.py
@@ -7,32 +7,39 @@ Uso: python pca.py imagen.png
 
 Notas: Es necesario contar con los modulos mdp, numpy y PIL.
 '''
-
-
 import mdp
 import numpy as np
 import random
 import Image
 
-im = Image.open("output.png")
-pix = im.load()
-w, h = im.size
-x = []
+f = open('muestras.dat', 'w')
+abec = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-for i in range(h):
-  tmp = []
-  for j in range(w):
-    if pix[j, i] == 255:
-      tmp.append(1)
-    else:
-      tmp.append(0)
-  x.append(tmp)
 
-# perform PCA on some data x
-y = mdp.pca(np.array(x,dtype = np.float64 ), output_dim = 1)
-for vect in x:
-  print vect
-suma = 0
-for value in y:
-  suma += value
-print value
+correcto = ['P', 'W', '3', '7', '6', '8', '8', 'D', 'Z', 'G', '2', '0', '1', 'R', 'S', '2', '0', '0']
+for k in range(len(correcto)):
+	im = Image.open("muestras/%s.png"%k)
+	pix = im.load()
+	w, h = im.size
+	x = []
+
+	for i in range(w):
+	  tmp = []
+	  for j in range(h):
+	    if pix[i, j] == (255, 255, 255):
+	      tmp.append(1)
+	    else:
+	      tmp.append(0)
+	  x.append(tmp)
+	
+	y = mdp.pca(np.array(x,dtype = np.float64 ), output_dim = (7))
+	y = y.transpose()
+	res = []
+	for value in y:
+		res.append(value.sum()*1.0e+14)
+	y = res
+	f.write("%s "%bin(abec.index(correcto[k]))[2:].zfill(7))
+	for value in y:
+	  f.write("%s "%(str(value))) 
+	f.write("\n")
+	#print "Dato para la imagen %s: %s\n"%(k, y)
